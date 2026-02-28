9a880ac3928098ccc6f46c5c5ed74b15103b7203
niqho <119586163+nvdema@users.noreply.github.com>
2026-02-28 16:07:28 -0500

    Key Sound FX
    Sound Effects added to Key Pickup

--- Team2_HallwayWithGuard/Assets/KMscripts/KeyPickup.cs
+++ Team2_HallwayWithGuard/Assets/KMscripts/KeyPickup.cs
@@ -3,44 +3,40 @@ using UnityEngine;
 
 public class KeyPickup : MonoBehaviour
 {
-
-
     private Inventory inventory;
     public GameObject keyText;
     public bool hasKey = false;
 
+    private AudioSource audisoSource;
+
+    void Start()
+    {
+         keyText.SetActive(false);
+        audisoSource = GetComponent<AudioSource>();
+    }
 
     // if collided with player, do AddKey method in inventory script, destroy the key and add it to inventory
     void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Player"))
         {
             inventory = other.GetComponent<Inventory>();
             inventory.AddKey();
             hasKey = true;
-            Destroy(gameObject);
+
             keyText.SetActive(true);
             //Debug.Log(" picked up key ");
+
+            audisoSource.Play();
+
+            GetComponent<Collider>().enabled = false;
+            
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+            {
+                r.enabled = false;
+            }
+
+            Destroy(gameObject, audisoSource.clip.length);
         }
-    }
-
-
-
-
-
-
-    void Start()
-    {
-        
-        
-        keyText.SetActive(false);
-
-
-    }
-
-    void Update()
-    {
-        
     }
 }
